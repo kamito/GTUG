bd9b30d79cc7c151a7421ed368650bb953310d56
Shinichirow Kamito <shinichirow@kmt.bz>
2011-01-20 00:21:45 +0900

    create GTUG 0 application.

---
+++ application/gtug0/models.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# gtug0.models
+
+from google.appengine.ext import db
+
+# Create your models here.
---
+++ application/gtug0/urls.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# gtug0.urls
+# 
+
+# Following few lines is an example urlmapping with an older interface.
+"""
+from werkzeug.routing import EndpointPrefix, Rule
+
+def make_rules():
+  return [
+    EndpointPrefix('gtug0/', [
+      Rule('/', endpoint='index'),
+    ]),
+  ]
+
+all_views = {
+  'gtug0/index': 'gtug0.views.index',
+}
+"""
+
+from kay.routing import (
+  ViewGroup, Rule
+)
+
+view_groups = [
+  ViewGroup(
+    Rule('/', endpoint='index', view='gtug0.views.index'),
+  )
+]
+
